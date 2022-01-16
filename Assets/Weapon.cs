b648e5930e1b462d1e803fd79ef2b4d243b83281
MaxenceDo <97642529+MaxenceDo@users.noreply.github.com>
2022-01-16 03:14:09 +0100

    TirPlusreal

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -8,6 +8,10 @@ public class Weapon : MonoBehaviour
     public float fireRate = 0;
     public float Damage = 10;
     public LayerMask whatToHit;
+
+    public Transform BulletTrailPrefab;
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
     float timeToFire = 0;
     Transform firePoint;
     // Start is called before the first frame update
@@ -46,6 +50,12 @@ public class Weapon : MonoBehaviour
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        if(Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+  
         Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100);
         if (hit.collider != null)
         {
@@ -53,4 +63,11 @@ public class Weapon : MonoBehaviour
             Debug.Log("We Hit " + hit.collider.name + "and did" + Damage + "Damage");
         }
     }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+    }
+
+
 }
