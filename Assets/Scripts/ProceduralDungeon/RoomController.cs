c8637c2716d4c3667dd08148b127195b248ffd81
MaxenceDo <97642529+MaxenceDo@users.noreply.github.com>
2022-01-21 15:26:17 +0100

    DebutDungeonGenerator

---
+++ Assets/Scripts/ProceduralDungeon/RoomController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class RoomInfo
+{
+    public string name;
+    public int X;
+    public int Y;
+}
+
+
+public class RoomController : MonoBehaviour
+{
+
+    public static RoomController instance;
+
+    string currentWorldName = "Basement";
+
+    RoomInfo currentLoadRoomData;
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+
+    public List<Room> loadedRooms = new List<Room>();
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
+    }
+}
