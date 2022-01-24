4651b03531522fcbc3a0077155148e6971b27e8c
MaxenceDo <97642529+MaxenceDo@users.noreply.github.com>
2022-01-24 07:36:51 +0100

    DungeonProcéduralP2

--- Assets/Scripts/ProceduralDungeon/RoomController.cs
+++ Assets/Scripts/ProceduralDungeon/RoomController.cs
@@ -21,9 +21,16 @@ public class RoomController : MonoBehaviour
 
     RoomInfo currentLoadRoomData;
 
+    Room currRoom;
+
     Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
 
     public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+    bool spawnedBossRoom = false;
+    bool updatedRooms = false;
+
     // Start is called before the first frame update
 
     void Awake()
@@ -31,9 +38,117 @@ public class RoomController : MonoBehaviour
         instance = this;
     }
 
+    void Start()
+    {
+        //LoadRoom("Start", 0, 0);
+        //LoadRoom("Empty", 1, 0);
+        //LoadRoom("Empty", -1, 0);
+        //LoadRoom("Empty", 0, 1);
+        //LoadRoom("Empty", 0, -1);
+    }
+
+    void Update()
+    {
+        UpdateRoomQueue();
+    }
+
+    void UpdateRoomQueue()
+    {
+        if (isLoadingRoom)
+        {
+            return;
+        }
+
+        if (loadRoomQueue.Count == 0)
+        {
+            return;
+        }
+
+        currentLoadRoomData = loadRoomQueue.Dequeue();
+        isLoadingRoom = true;
+        StartCoroutine(LoadRoomRoutine(currentLoadRoomData));
+
+
+
+    }
+
+
+
+    public void LoadRoom(string name, int x, int y)
+    {
+        if (DoesRoomExist(x, y) == true)
+        {
+            return;
+        }
+
+        RoomInfo newRoomData = new RoomInfo();
+        newRoomData.name = name;
+        newRoomData.X = x;
+        newRoomData.Y = y;
+
+        loadRoomQueue.Enqueue(newRoomData);
+    }
+
+
+    IEnumerator LoadRoomRoutine(RoomInfo info)
+    {
+        string roomName = currentWorldName + info.name;
+
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+
+        while (loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        if (!DoesRoomExist(currentLoadRoomData.X, currentLoadRoomData.Y))
+        {
+            room.transform.position = new Vector3(
+                currentLoadRoomData.X * room.Width,
+                currentLoadRoomData.Y * room.Height,
+                0
+            );
+
+            room.X = currentLoadRoomData.X;
+            room.Y = currentLoadRoomData.Y;
+            room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
+            room.transform.parent = transform;
+
+            isLoadingRoom = false;
+
+            if (loadedRooms.Count == 0)
+            {
+                CameraController.instance.currRoom = room;
+            }
+
+            loadedRooms.Add(room);
+            room.RemoveUnconnectedDoors();
+        }
+        else
+        {
+            Destroy(room.gameObject);
+            isLoadingRoom = false;
+        }
+
+    }
 
     public bool DoesRoomExist(int x, int y)
     {
         return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
+    }
+
+    public Room FindRoom(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y);
+    }
+
+    public void OnPlayerEnterRoom(Room room)
+    {
+        CameraController.instance.currRoom = room;
+        currRoom = room;
+
     }
 }
